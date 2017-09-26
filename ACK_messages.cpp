ab336edd0d41a7031723d5e715be3a586e004b02
Zach <zrb@umd.edu>
2017-09-26 17:52:40 -0400

    formatted last file

--- ACK_messages.cpp
+++ ACK_messages.cpp
@@ -1,87 +1,81 @@
 #include "Arduino.h"
 #include "BPPMAXGNSS.h"
 
-namespace BPPMAXGNSS {
+namespace BPPMAXGNSS
+{
+    ACK_ACK::ACK_ACK(void)
+    {
+        _msgClass = CLASS_ACK;
+        _msgID = ID_ACK_ACK;
+        _dataLen = 2;    // Length of data
 
-  ACK_ACK::ACK_ACK(void){
-    _msgClass = CLASS_ACK;
-		_msgID = ID_ACK_ACK;
-		_dataLen = 2; // Length of data
+        //Message-specific fields
 
-		//Message-specific fields
+        clsID = 0;
+        msgID = 0;
+    }
 
-    clsID = 0;
-    msgID = 0;
+    void ACK_ACK::encodeMsg(uint8_t* buf)
+    {
+        uint8_t temp[4];
 
-  }
+        //Encode clsID
+        encodeU1(temp, clsID);
+        buf[NUM_HEADER_BYTES + 0] = temp[0];
 
-  void ACK_ACK::encodeMsg(uint8_t* buf){
-    uint8_t temp[4];
+        //Encode msgID
+        encodeU1(temp, msgID);
+        buf[NUM_HEADER_BYTES + 1] = temp[0];
+    }
 
-    //Encode clsID
-    encodeU1(temp,clsID);
-    buf[NUM_HEADER_BYTES + 0] = temp[0];
+    void ACK_ACK::decodeMsg(uint8_t* buf)
+    {
+        uint8_t temp[4];
 
-    //Encode msgID
-    encodeU1(temp,msgID);
-    buf[NUM_HEADER_BYTES + 1] = temp[0];
+        //Decode clsID
+        temp[0] = buf[NUM_HEADER_BYTES + 0];
+        clsID = decodeU1(temp);
 
-  }
+        //Decode msgID
+        temp[0] = buf[NUM_HEADER_BYTES + 1];
+        msgID = decodeU1(temp);
+    }
 
-  void ACK_ACK::decodeMsg(uint8_t* buf){
-    uint8_t temp[4];
+    ACK_NACK::ACK_NACK(void)
+    {
+        _msgClass = CLASS_ACK;
+        _msgID = ID_ACK_NACK;
+        _dataLen = 2;    // Length of data
 
-    //Decode clsID
-		temp[0] = buf[NUM_HEADER_BYTES + 0];
-		clsID = decodeU1(temp);
+        //Message-specific fields
 
-    //Decode msgID
-		temp[0] = buf[NUM_HEADER_BYTES + 1];
-		msgID = decodeU1(temp);
+        clsID = 0;
+        msgID = 0;
+    }
 
-  }
+    void ACK_NACK::encodeMsg(uint8_t* buf)
+    {
+        uint8_t temp[4];
 
-  ACK_NACK::ACK_NACK(void){
-    _msgClass = CLASS_ACK;
-		_msgID = ID_ACK_NACK;
-		_dataLen = 2; // Length of data
+        //Encode clsID
+        encodeU1(temp, clsID);
+        buf[NUM_HEADER_BYTES + 0] = temp[0];
 
-		//Message-specific fields
+        //Encode msgID
+        encodeU1(temp, msgID);
+        buf[NUM_HEADER_BYTES + 1] = temp[0];
+    }
 
-    clsID = 0;
-    msgID = 0;
+    void ACK_NACK::decodeMsg(uint8_t* buf)
+    {
+        uint8_t temp[4];
 
-  }
+        //Decode clsID
+        temp[0] = buf[NUM_HEADER_BYTES + 0];
+        clsID = decodeU1(temp);
 
-  void ACK_NACK::encodeMsg(uint8_t* buf){
-    uint8_t temp[4];
-
-    //Encode clsID
-    encodeU1(temp,clsID);
-    buf[NUM_HEADER_BYTES + 0] = temp[0];
-
-    //Encode msgID
-    encodeU1(temp,msgID);
-    buf[NUM_HEADER_BYTES + 1] = temp[0];
-
-  }
-
-  void ACK_NACK::decodeMsg(uint8_t* buf){
-    uint8_t temp[4];
-
-    //Decode clsID
-		temp[0] = buf[NUM_HEADER_BYTES + 0];
-		clsID = decodeU1(temp);
-
-    //Decode msgID
-		temp[0] = buf[NUM_HEADER_BYTES + 1];
-		msgID = decodeU1(temp);
-
-  }
-
-
-
-
-
-
+        //Decode msgID
+        temp[0] = buf[NUM_HEADER_BYTES + 1];
+        msgID = decodeU1(temp);
+    }
 }
