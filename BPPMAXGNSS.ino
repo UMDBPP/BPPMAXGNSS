2f2fbdbbedc8887f798ab8d9ed1ae3a85bf973c6
Zach <zrb@umd.edu>
2017-09-26 17:06:13 -0400

    added .ino file for testing library functions

---
+++ BPPMAXGNSS.ino
@@ -0,0 +1,13 @@
+// For testing BPPMAXGNSS library
+#include "Arduino.h"
+#include "BPPMAXGNSS.h"
+
+void setup()
+{
+// Add your initialization code here
+}
+
+void loop()
+{
+//Add your repeated code here
+}
